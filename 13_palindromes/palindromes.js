22e7237a64f46bc1e5b569d6101c77c08109dd45
tithoft <99927615+tithoft@users.noreply.github.com>
2025-09-24 14:14:33 -0400

    Completed palindrome functions by removing non
    alphanumeric characters and creating new string

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reverseSting = cleanString.split('').reverse().join('');
+    return cleanString === reverseSting;
 };
 
 // Do not edit below this line
